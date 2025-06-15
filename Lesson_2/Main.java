f842e5e7161c291e98c9549feebe8d9262e433c7
tagievsamir <tagievsamir@mail.ru>
2025-06-15 22:55:21 +0300

    Added 2new classes(printThreeWords,checkSumSign)

--- Lesson_2/Main.java
+++ Lesson_2/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+
+    }
+    public static void printThreeWords() {
+        System.out.println ("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign(){
+        int a = 1;
+        int b = 2;
+        if ((a + b) >= 0){
+                System.out.println("Сумма положительная");
+        }else {
+                System.out.println("Сумма отрицательная");
+        }
+    }
 }
